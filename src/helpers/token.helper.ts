29268adc066f68f737b0dd844053bc0f892a11c6
Dmytro Dobrovolskyi <dimadobrovolski@gmail.com>
2026-06-11 20:45:57 +0300

    Add user info

--- src/helpers/token.helper.ts
+++ src/helpers/token.helper.ts
@@ -1,6 +1,10 @@
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config/app.config.js';
 
-export function createToken(user: { id: string; role: string }) {
-    return jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, { expiresIn: '7d' });
+export function createToken(user: { id: string; role: string; name: string; surname: string; email: string }) {
+    return jwt.sign(
+        { id: user.id, role: user.role, name: user.name, surname: user.surname, email: user.email },
+        JWT_SECRET,
+        { expiresIn: '7d' }
+    );
 }
