66680cd8ee5d306f5d783400d4304dafb655ea04
Dmytro Dobrovolskyi <dimadobrovolski@gmail.com>
2026-06-21 01:46:30 +0300

    Fix swagger config

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -141,7 +141,7 @@ const options: swaggerJsdoc.Options = {
             },
         },
     },
-    apis: ['./src/routes/*.ts'],
+    apis: ['./src/routes/*.ts', './dist/routes/*.js'],
 };
 
 export const swaggerSpec = swaggerJsdoc(options);
